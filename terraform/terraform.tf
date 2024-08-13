927b50e28391b8981bd02c781b3c4ac7abfb786f
sohil <sohil43patel@gmail.com>
2024-08-13 07:05:24 +0530

    changes in variable names

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -27,9 +27,9 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "gitopsterrastate"
+    bucket = "vprofileaction43"
     key    = "terraform.tfstate"
-    region = "us-east-2"
+    region = "ap-northeast-3"
   }
 
   required_version = "~> 1.6.3"
